e9e57acc0ae8ba8d40aef3d79fec7f262dd1b87d
Linus Sjögren <thelinx@unreliablepollution.net>
2010-06-15 14:15:27 +0200

    Now actually works\!

--- ualove/init.lua
+++ ualove/init.lua
@@ -4,11 +4,12 @@ function hook.add(event, func, id, state, debugonly)
 	assert(type(event) == "string", "bad argument #1 to hook.add (string expected, got "..type(event)..")")
 	assert(type(func) == "function", "bad argument #2 to hook.add (function expected, got "..type(func)..")")
 	assert(type(id) == "string", "bad argument #3 to hook.add (string expected, got "..type(func)..")")
+	local func = func
 	if state ~= nil then
 		assert(type(state) == "string", "bad argument #4 to hook.add (string expected, got "..type(func)..")")
-		local oldfunc = func
-		function func(...)
-			if game.state == state then
+		local oldfunc,s = func,state
+		func = function(...)
+			if _G.game.state == s then
 				return oldfunc(...)
 			end
 			return true
@@ -16,9 +17,9 @@ function hook.add(event, func, id, state, debugonly)
 	end
 	if debugonly ~= nil then
 		assert(type(debugonly) == "boolean", "bad argument #5 to hook.add (boolean expected, got "..type(func)..")")
-		local oldfunc = func
-		function func(...)
-			if game.debug == debugonly then
+		local oldfunc,dbg = func,debugonly
+		func = function(...)
+			if _G.game.debug == dbg then
 				return oldfunc(...)
 			end
 			return true
@@ -28,12 +29,12 @@ function hook.add(event, func, id, state, debugonly)
 end
 
 function hook.call(event, ...)
-	out = {}
+	local queue,out = {},{}
 	for k,v in pairs(game.hooks) do
 		if v[1] == event then
-			success,ret = v[2](...)
-			if not success then
-				error("Hook "..k.." failed! Error: "..ret)
+			local ret,err = v[2](...)
+			if ret == false then
+				error("Hook "..k.." failed! Error: "..err)
 			else
 				if ret then
 					table.insert(out, ret)
@@ -45,9 +46,23 @@ function hook.call(event, ...)
 end
 
 function love.run()
+	if love.graphics then
+		love.graphics.clear()
+	end
+	hook.call("initial")
+	if love.graphics then
+		love.graphics.present()
+	end
+
 	hook.call("load")
 
 	local dt = 0
+
+	if love.audio then
+		hook.add("quit", function()
+			love.audio.stop()
+		end, "audioquitcheck")
+	end
 
 	while true do
 		if love.timer then
@@ -55,14 +70,24 @@ function love.run()
 			dt = love.timer.getDelta()
 		end
 		hook.call("update", dt)
-		if love.graphics then
-			love.graphics.clear()
-			hook.call("draw")
-		end
+
+        if love.event then
+            for e,a,b,c in love.event.poll() do
+				if e == "q" then
+					game.quit = true
+				end
+                love.handlers[e](a,b,c)
+            end
+        end
 
 		if game.quit == true then
 			hook.call("quit")
 			return
+		end
+
+		if love.graphics then
+			love.graphics.clear()
+			hook.call("draw")
 		end
 
 		if love.graphics then
@@ -76,6 +101,7 @@ end
 
 for _,n in pairs{"joystickpressed", "joystickreleased", "keypressed",
  "keyreleased", "mousepressed", "mousereleased"} do
+	local n = n
 	love[n] = function(...)
 		return hook.call(n, ...)
 	end
